5775aea20667c1adbd7b913a15563b5c557d9f02
zzh <1152174477@qq.com>
2022-06-20 12:38:20 +0800

    final version zhxy

--- src/main/java/com/atguigu/myzhxy/MyzhxyApplication.java
+++ src/main/java/com/atguigu/myzhxy/MyzhxyApplication.java
@@ -8,11 +8,7 @@ public class MyzhxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyzhxyApplication.class, args);
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push-test");
+
     }
 
 }
